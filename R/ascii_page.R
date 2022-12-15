3db9d83ffe938e8dc818fe78ef5b206277ea0091
louis.heraut <louis.heraut@inrae.fr>
2022-12-15 17:02:04 +0100

    cm

---
+++ R/ascii_page.R
@@ -0,0 +1,40 @@
+# Copyright 2022 Louis Héraut (louis.heraut@inrae.fr)*1,
+#                Éric Sauquet (eric.sauquet@inrae.fr)*1
+#
+# *1   INRAE, France
+#
+# This file is part of dataSheep R package.
+#
+# dataSheep R package is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# dataSheep R package is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with dataSheep R package.
+# If not, see <https://www.gnu.org/licenses/>.
+
+
+# ascii_page = function (NAME, HEIGHT, WIDTH) {
+
+#     Name = as.vector(NAME)
+#     Name = Name[!duplicated(Name)]
+#     Letter = Name
+#     Letter[grepl("margin", Name)] = "#"
+#     Letter[grepl("just", Name)] = "@"
+#     Letter[grepl("void", Name)] = "."
+
+    
+#     page = 
+    
+    
+# }
+
+NAME = matrix(c("Aafde", "Bvze", "void", "De", "Bfe", "Fzae"), ncol=2)
+HEIGHT = matrix(c(1, 2, 1, 1, 2, 1), ncol=2)
+WIDTH = matrix(c(1, 1.5, 2, 2, 1.5, 2), ncol=2)
